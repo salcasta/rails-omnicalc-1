024e0df5c86c7928a90d9f1ce1aa53cd4f0aff42
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-07 18:07:19 +0000

    main page/square calc and square result page complete

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -2,4 +2,11 @@ class MathController < ApplicationController
   def main_page
     render ({ :template => "math_templates/square_new"})
   end
+
+  def square_result
+    @the_num = params.fetch("number").to_f
+    @the_result = @the_num ** 2 
+    
+    render ({ :template => "math_templates/square_result"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get("/", :controller => "math", :action: "main_page")
+  get("/", :controller => "math", :action => "main_page")
+  get("/square/results", :controller => "math", :action => "square_result")
 end
