511fdab6649039c5090a3b43f8dccdf24d6f8351
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-07 18:23:13 +0000

    sqaure root page and square root result page complete

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -6,7 +6,19 @@ class MathController < ApplicationController
   def square_result
     @the_num = params.fetch("number").to_f
     @the_result = @the_num ** 2 
-    
+
     render ({ :template => "math_templates/square_result"})
+  end
+
+  def square_root_form
+
+    render ({ :template => "math_templates/square_root_form"})
+  end
+
+  def square_root_result
+    @the_num = params.fetch("number").to_f
+    @the_result = Math.sqrt(@the_num)
+
+    render ({ :template => "math_templates/square_root_result"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", :controller => "math", :action => "main_page")
   get("/square/results", :controller => "math", :action => "square_result")
+
+  get("/square_root/new", :controller => "math", :action => "square_root_form")
+  get("/square_root/results", :controller => "math", :action => "square_root_result")
 end
